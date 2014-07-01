ce06409e801842dd2b5ffd38179777d042f0b9af
Wérnedres <wernedres@hotmail.com>
2014-07-01 12:43:48 -0300

    [ADJUST]form contato modificado
    Signed-off-by: Wérnedres <“wernedres@hotmail.com”>

--- forms/formContato.php
+++ forms/formContato.php
@@ -1,74 +1,140 @@
 
-<?php include_once  '.../header.php'; ?>
- <?php include_once '../menu.php'; ?>
-<?php include_once  '../pg_connect.php';?>
+<?php include_once 'header.php'; ?>
+ <?php include_once 'menu.php'; ?>
+<?php include_once 'pg_connect.php';?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>formulario de teste contato</title>
-</head>
+  <?php 
 
+            $nome      = strip_tags (trim($_POST['nome']));
+            $telefone  = strip_tags (trim($_POST['telefone']));
+            $email     = strip_tags (trim($_POST['email']));
+            $endereco  = strip_tags (trim($_POST['endereco']));
+            $numero    = strip_tags (trim($_POST['numero']));
+            $bairro    = strip_tags (trim($_POST['bairro']));
+            $profissao = strip_tags (trim($_POST['profissao']));
 
+            if(isset($_POST['enviar'])){         
+            $pg_inserir = 'INSERT INTO contatos(cont_nome,cont_telefone,cont_email,cont_endereco,cont_numero,cont_bairro,cont_profissao)';
+            $pg_inserir .= 'values (:nome,:telefone,:email, :endereco, :numero, :bairro, :profissao)';
 
-		<?php
-
-		$nome      = strip_tags(trim($_POST['nome']));
-		$telefone  = strip_tags(trim($_POST['telefone']));
-		$email     = strip_tags(trim($_POST['email']));
-		$endereco  = strip_tags(trim($_POST['endereco']));
-		$numero    = strip_tags(trim($_POST['numero']));
-		$bairro    = strip_tags(trim($_POST['bairro']));
-		$profissao = strip_tags(trim($_POST['profissao']));
-
-     if(isset($_POST['enviar'])){
-
-     $pg_inserir = 'INSERT INTO contatos (cont_nome, cont_telefone,cont_email,cont_endereco,cont_numero,cont_bairro,cont_profissao)';
-     $pg_inserir .= 'values(:nome, :telefone, :email,:endereco,:numero,:bairro,:profissao)';
  
-	try{
-		$query_inserir = $db->prepare($pg_inserir);
-		$query_inserir->bindValue(':nome',$nome,PDO::PARAM_STR);
-		$query_inserir->bindValue(':telefone',$telefone,PDO::PARAM_STR);
-	    $query_inserir->bindValue(':email',$email,PDO::PARAM_STR);
-		$query_inserir->bindValue(':endereco',$endereco,PDO::PARAM_STR);
-	   	$query_inserir->bindValue(':numero',$numero,PDO::PARAM_STR);
-	   	$query_inserir->bindValue(':bairro',$bairro,PDO::PARAM_STR);
-        $query_inserir->bindValue(':profissao',$profissao,PDO::PARAM_STR);
-        $query_inserir->execute();
 
-      
-      echo "<script>alert('Usuario cadastrado com sucesso')</script>";
+            try{  
+            $pg_query=$db->prepare($pg_inserir);
+            $pg_query->bindValue(':nome',$nome,PDO::PARAM_STR);
+            $pg_query->bindValue(':telefone',$telefone,PDO::PARAM_STR);
+            $pg_query->bindValue(':email',$email,PDO::PARAM_STR);
+            $pg_query->bindValue(':endereco',$endereco,PDO::PARAM_STR);
+            $pg_query->bindValue(':numero',$numero,PDO::PARAM_STR);
+            $pg_query->bindValue(':bairro',$bairro,PDO::PARAM_STR);
+            $pg_query->bindValue(':profissao',$profissao,PDO::PARAM_STR);
+            $pg_query->execute();
 
-	}catch (PDOexception $erro_cadastro){
-		echo "erro ao  cadastrar".$erro_cadastro->getMessage;
-	}
+      }catch(PDOexception $e){
+        echo $e->getMessage();
 
+         }
 
+ # code...
 }
 
-   ?>
+ ?>
 
 
-<body>
 
 
-    <form action"" method="post">
 
-	<label>NOME:<input type="text" name="nome"/></label><br/>
-	<label>TELEFONE:<input type="text" name="telefone"/></label><br/>
-	<label>Email:<input type="text" name="email"/></label><br/>
-	<label>ENDERECO:<input type="text" name="endereco"/></label><br/>
-	<label>NUMERO:<input type="text" name="numero"></label><br/>
-	<label>BAIRRO:<input type="text" name="bairro"></label><br/>
-	<label>PROFISSAO:<input type="text" name="profissao"/></label><br/>
-	<input type="submit" name="enviar" value="Cadastrar"/>
-	
 
-	</form>
 
-<?php include_once '../footer.php'; ?>
 
-</body>
-</html>
+
+<div class="container"> 
+        <div class="jumbotron">
+
+    <h4 class="alert alert-success">Cadastrar Contato</h4>
+            <hr/><br/>
+
+
+            <form   class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">Nome</label>
+                       <div class="col-sm-4">
+                           <input name="nome"  type="text"  placeholder="Digite a nome" required>
+
+                       </div> 
+                </div>
+
+    <div class="form-group">
+                <label class="col-sm-2 control-label">Telefone</label>
+                    <div class="col-sm-3">
+                        <input name="telefone" type="text"  value="" placeholder="Digite a telefone" required>
+                     </div>
+            </div>
+
+
+
+
+
+            <div class="form-group">
+              <label class="col-sm-2 control-label">Email</label>
+                    <div class="col-sm-3">
+                       <input name="email" type="text"  placeholder="Digite o email"  onBlur="ValidaEmail();" required>
+                    </div>
+             </div>
+
+
+              <div class="form-group">
+                  <label class="col-sm-2 control-label">Endereço</label>
+                       <div class="col-sm-3">
+                           <input name="endereco"  type="text"  placeholder="Digite o endereço " required>
+
+                        </div>
+                </div>
+
+
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Numero</label>
+                    <div class="col-sm-3">
+                        <input name="numero" type="text"  value="" placeholder="Digite o endereço" required>
+                     </div>
+            </div>
+
+
+            <div class="form-group">
+                <label class="col-sm-2 control-label">Bairro</label>
+                    <div class="col-sm-3">
+                        <input name="bairro" type="text"  value="" placeholder="Digite o endereço" required>
+                     </div>
+            </div>
+
+
+
+                             <div class="form-group">
+                  <label class="col-sm-2 control-label">Profissao</label>
+                       <div class="col-sm-3">
+                           <input name="profissao"  type="text"  placeholder="Digite a profissao " required>
+
+                        </div>
+                </div>
+
+
+                <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-7">
+                        <input type="submit" name="enviar" value="Cadastrar" class="btn btn-success"/>
+                    </div>
+                </div>
+        
+            </form>
+</div>
+
+</div>
+</div>
+<?php require_once("footer.php"); 
+
+
+
+
+
+
